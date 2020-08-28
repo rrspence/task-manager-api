ea42bb7ea1617697dbfb40d51425fdf019aa0b77
rrspence <spencer.rahim@gmail.com>
2020-08-27 20:31:58 -0600

    Init commit

---
+++ mongodb.js
@@ -0,0 +1,34 @@
+// CRUD - create, read, update, delete
+
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database.');
+    }
+
+   const db = client.db(databaseName);
+   
+//    db.collection('users').deleteMany({ 
+//        age: 30 
+//     }).then((result) => {
+//         console.log(result);
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+
+    db.collection('tasks').deleteOne({ 
+       description: 'Clean the house' 
+     }).then((result) => {
+         console.log(result);
+     }).catch((error) => {
+         console.log(error);
+     })
+});
